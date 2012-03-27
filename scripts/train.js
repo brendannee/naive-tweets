0795192c616d774c14bd686c39e14fe76ad0c302
Brendan Nee <brendan.nee@gmail.com>
2012-03-27 14:06:44 -0400

    Added npm run-script to train tweets

--- scripts/train.js
+++ scripts/train.js
@@ -3,6 +3,8 @@ try{
 } catch(e) { }
 
 var models = require('../models/models')
+  , ProgressBar = require('progress')
+  , _ = require('underscore')
   , async = require('async')
   , languages = require('../lib/languages')
   , mongoose = require('mongoose')
@@ -19,40 +21,147 @@ var models = require('../models/models')
     });
 
 
-// for each language, train it
-async.forEachSeries(languages, trainLanguage, function(e, results){
-  console.log('All Languages Trained');
+console.log('Staring Training - This takes a while.');
+async.series([
+    getTrainingData
+  , clearProbabilities
+  , countWords
+  , calculateWordProbabilities
+  , classifyTweets
+], function(e, results){
+  console.log('All Tweets Classified');
   process.exit();
 });
 
-function trainLanguage(language, cb){
-  try{
-    twit.search('place:' + language.loc, {rpp:100}, function(err, data) {
-      console.log('Training for ' + language.name + ': ' + data.results.length + ' tweets');
-      async.forEachSeries(data.results, processTweet, function(e, results){
+
+function getTrainingData(cb){
+  // for each language, train it
+  async.forEachSeries(languages, trainLanguage, cb);
+
+  function trainLanguage(language, cb){
+    if(language.loc){
+      twit.search('place:' + language.loc, {rpp:100}, function(err, data) {
+        console.log('Training for ' + language.name + ': ' + data.results.length + ' tweets');
+        async.forEachSeries(data.results, processTweet, function(e, results){
+          cb();
+        });
+      });
+    }
+  }
+
+
+  function processTweet(data, cb){
+    try{
+      //classify tweet based on language
+      var tweet = new Tweet(data);
+
+      tweet.trained_language = language.code; 
+      tweet.trained = true;
+      tweet.autotrained = true;
+      tweet.save(function(e, result){
         cb();
       });
-
-    });
-  } catch(e) {
-    cb();
+    } catch(e) {
+      cb();
+    }
   }
 }
 
 
-function processTweet(data, cb){
-  try{
-    //classify tweet based on language
-    var tweet = new Tweet(data);
+function clearProbabilities(cb){
+  Probability.collection.drop(cb);
+}
 
-    tweet.trained_language = language.code; 
-    tweet.trained = true;
-    tweet.autotrained = true;
-    tweet.save(function(e, result){
+function countWords(cb){
+  //Find all trained tweets, break them into words and count them
+
+  var bar = new ProgressBar('Processing tweets [:bar] :percent :etas', {
+      complete: '='
+    , incomplete: ' '
+    , width: 20
+    , total: 100
+  });
+
+  Tweet.find({trained: true}, function(e, tweets){
+    bar.total = tweets.length;
+    async.forEachSeries(tweets, parseTweet, cb);
+  });
+
+  function parseTweet(tweet, cb){
+    async.forEach(tweet.getWords(), function(word, cb){
+      if(word){
+        var updateField = "count." + tweet.trained_language
+          , update = {$inc: {}};
+        update.$inc[updateField] = 1;
+        Probability.update({word: word}, update, {upsert: true}, cb);
+      }
+    }, function(e, results){
+      bar.tick();
       cb();
     });
-  } catch(e) {
-    cb();
   }
 }
+
+function calculateWordProbabilities(cb){
+  //Get a list of all words and put into probabilities table
+
+  var bar = new ProgressBar('Calculating probabilities [:bar] :percent :etas', {
+      complete: '='
+    , incomplete: ' '
+    , width: 20
+    , total: 100
+  });
+
+  Probability.find({}, function(e, words){
+    bar.total = words.length;
+    async.forEach(words, calculateWord, cb);
+  });
+  
+  function calculateWord(word, cb){
+    var wordCount = {}
+      , tweetCount = {}
+      , totalWordCount
+      , totalTweetCount
+      , probability = {};
+
+    //Get counts for each tweet and word
+    async.forEach(languages, function(language, cb){
+      wordCount[language.code] = word.count[language.code] || 0;
+      Tweet.count({ trained_language: language.code }, function(e, count){
+        tweetCount[language.code] = count;
+        cb();
+      });
+    }, function(e, results){
+      bar.tick();
+      //calculate probabilities
+      totalWordCount = _.reduce(wordCount, function(memo, num){ return memo + num; }, 0);
+      totalTweetCount = _.reduce(tweetCount, function(memo, num){ return memo + num; }, 0);
+
+      if(totalWordCount >= 3){
+        //if word occurs at least 3 times, then use it to calculate probability
+        //Minimum probability of 0.01
+
+        languages.forEach(function(language){
+          word.probability[language.code] = Math.max(0.01, ( wordCount[language.code] / tweetCount[language.code] ) / ( ( wordCount[language.code] / tweetCount[language.code] ) + ( ( totalWordCount - wordCount[language.code] ) / ( totalTweetCount - tweetCount[language.code] ) ) ) ) || 0.01;
+        });
+
+        //save probabilities
+        word.save(cb);
+      } else {
+        //word not common enough to use
+        cb();
+      }
+    });
+  }
+}
+
+function classifyTweets(cb){
+  //classify each tweet
+  Tweet.find({}, function(e, tweets){
+    async.forEach(tweets, function(tweet, cb){
+      tweet.classify(cb);
+    }, cb);
+  });
+}
+
 
